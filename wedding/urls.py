d4e5a069d1cbe53d65ec005bd7a6a1d5865e11a5
maram <maram001benrhouma@gmail.com>
2025-04-20 18:16:06 +0100

    Configuration de l'Administration

--- wedding/urls.py
+++ wedding/urls.py
@@ -6,3 +6,7 @@ app_name = 'wedding'
 urlpatterns = [
     path('', views.liste_articles, name='liste_articles'),
 ]
+urlpatterns = [
+    path('', views.liste_articles, name='liste_articles'),
+    path('article/<int:article_id>/', views.detail_article, name='detail_article'),
+]
--- wedding/views.py
+++ wedding/views.py
@@ -4,3 +4,8 @@ from .models import Article
 def liste_articles(request):
     articles = Article.objects.all().order_by('-date_creation')
     return render(request, 'wedding/liste_articles.html', {'articles': articles})
+from django.shortcuts import render, get_object_or_404
+
+def detail_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'blog/detail_article.html', {'article': article})
